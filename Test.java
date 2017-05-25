56f821f415e28dce82d24ab4fb1626e4fcff86db
8608311411IIIT <sanjeevkumar181998@gmail.com>
2017-05-25 17:49:24 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test{
+     public void main(String []args){
+         static{
+               print(10);
+         }
+     }
+         static void print(int x){
+                System.out.println(x);
+                System.exit(0);
+         }
+           
+                
+    
+
+}
